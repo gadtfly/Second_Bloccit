a44d696cc164210dc8ab60874b7b91406d418680
Santos Lizcano <arquitectolizcano@gmail.com>
2014-06-03 17:51:07 -0500

    Cleaned up Registration Views and App Layout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
 
   def create 
     @topic = Topic.find(params[:topic_id])
-    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    @post = current_user.posts.build(post_params)
     @post.topic = @topic
     
     authorize @post 
@@ -41,13 +41,19 @@ class PostsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize @post 
-    if @post.update_attributes(params.require(:post).permit(:title, :body))
+    if @post.update_attributes(post_params)
       flash[:notice] = "Your Post was Updated!!!"
       redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving your post. Please try AGAIN."
       render :new
     end
+  end
+
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
 
