c71bc6083b495f280348922acda4515dbbe67eda
Santos Lizcano <arquitectolizcano@gmail.com>
2014-06-03 17:01:08 -0500

    Configured app for Production emails

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,45 @@
+class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+    authorize @topics 
+  end
+
+  def new
+    @topic = Topic.new
+    authorize @topic
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+    #cambiar o agregar?
+    @posts = @topic.posts
+    #authorize @topic
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+    authorize @topic 
+  end
+
+  def create
+    @topic = Topic.new(params.require(:topic).permit(:name, :description, :public))
+    authorize @topic
+    if @topic.save
+      redirect_to @topic, notice: "Topic was saved successfully."
+    else
+      flash[:error] = "There was an error creating topic, please try again!!!"
+      render :new
+    end
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+    authorize @topic 
+    if @topic.update_attributes(params.require(:topic).permit(:name, :description, :public))
+       redirect_to @topic 
+    else
+      flash[:error] = "There was an error saving topic, please try again!!!."
+      render :edit
+    end
+  end
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #http://secondbloccit.herokuapp.com
+  config.action_mailer.default_url_options = {host: 'secondbloccit.herokuapp.com' }
 end
