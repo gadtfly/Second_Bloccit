688ca240f989dc54f0e98c07bd633a7796839d41
Santos Lizcano <arquitectolizcano@gmail.com>
2014-06-11 19:27:57 -0500

    interlude changes without errors

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,12 +4,6 @@ class Vote < ActiveRecord::Base
   validates :value, inclusion: { in: [-1, 1], message: "%{value} is not a valid vote, change it now!." }
   after_save :update_post
 
-  private
-
-  def update_post
-    self.post.update_rank
-  end
-
   def up_vote?
     value == 1
   end
@@ -17,6 +11,11 @@ class Vote < ActiveRecord::Base
   def down_vote?
     value == -1
   end
-  
+
+  private
+
+  def update_post
+    self.post.update_rank
+  end
 
 end
