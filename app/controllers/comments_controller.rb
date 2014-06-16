c703691ebc2711052a4532b35620f7d99b7beff3
Santos Lizcano <arquitectolizcano@gmail.com>
2014-06-16 13:09:08 -0500

    Ajaxified comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,26 @@
  class CommentsController < ApplicationController
   respond_to :html, :js
+
    def create 
     @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.find(params[:post_id])
     @comments = @post.comments 
-    @comment = Comment.new(comment_params)
 
-    authorize @comment
-
-    @comment.user = current_user
+    @comment = current_user.comments.build( comment_params )
     @comment.post = @post
     @new_comment = Comment.new
+    
+    authorize @comment
 
     if @comment.save
-      flash[:notice] = "Comment was saved, its soul should be clean now."
+      flash[:notice] = "The comment was created successfully!"
     else
-      flash[:error] = "There was an error saving the comment, please purify its soul."
+      flash[:error] = "There was an error saving the comment, please try again."
     end
 
-    redirect_to topic_post_path(@topic, @post)
-
-    # respond_with(@comment) do |f|
-    #   f.html {redirect_to [@topic, @post] }
-    # end
+    respond_with(@comment) do |f|
+      f.html { redirect_to [@topic, @post] }
+    end
   end
 
   def update
@@ -61,6 +59,9 @@ end
 private
 
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(
+      :body,
+      :post_id
+      )
   end
 end
